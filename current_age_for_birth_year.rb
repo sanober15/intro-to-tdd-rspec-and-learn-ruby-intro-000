5c8e7702a8376255ce3d9f505269a290f3d9e2d0
sanober riaz <sanober@sanober.me>
2017-12-11 02:54:16 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -2,7 +2,7 @@ describe "current_age_for_birth_year" do
   it "returns the age of a person based on their year of birth" do
     age_of_person = current_age_for_birth_year(1984)
     expect(age_of_person).to eq(19)
-    
+
   end
   it "should return the current year for a person born in year 0" do
     twenty_sixteen = current_age_for_birth_year(0)
